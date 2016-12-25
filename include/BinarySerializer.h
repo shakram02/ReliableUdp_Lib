cb788d2016ed217d517656ad46ab191ac714e5e3
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2016-12-25 13:24:01 +0200

    documentation

--- include/BinarySerializer.h
+++ include/BinarySerializer.h
@@ -14,23 +14,25 @@ public:
     /**
      * Takes a data packet and converts it to raw binary data
      * @param data_pack Input data packet
-     * @param ptr Pointer to the output raw binary data
+     * @param projected_data_ptr Pointer to the re-projected raw ACK packet
      */
-    static void SerializeDataPacket(DataPacket *data_pack, void **ptr);
+    static void SerializeDataPacket(DataPacket *data_pack, void **projected_data_ptr);
 
     /**
      * Takes an ACK packet and converts it to raw binary data
      * @param ack_pack Input ACK packet
-     * @param ptr Pointer to the output raw binary data
+     * @param projected_ack_ptr Pointer to the re-projected raw ACK packet
      */
-    static void SerializeAckPacket(AckPacket *ack_pack, void **ptr);
+    static void SerializeAckPacket(AckPacket *ack_pack, void **projected_ack_ptr);
 
     /**
      * Takes an array of raw binary data and returns its equivalent Data Packet
      * Representing the DataPacket as a simple struct (non-pointer) alawys fails to work
      * so a (DataPacket**) must be passed to the function so a value of (DataPacket*) is set
-     * The user can then proceed the re-projected DataPacket only through its pointer
+     *
+     * The user should then proceed the re-projected DataPacket only through its pointer
      * That is a lesson that I learned the hard way.
+     *
      * @param packetBytes Raw binary data
      * @param packet_len Length of the binary data array
      * @return A reconstructed DataPacket from the provided binary data
@@ -42,8 +44,8 @@ public:
      * Takes an array of raw binary data and returns its equivalent ACK Packet
      * the length of the ack packet is constant
      * A AckPacket** must be passed to the function so the re-projection works correctly
-     * @param packetBytes Raw binary data
-     * @return A reconstructed ACK Packet from the provided binary data
+     * @param packetBytes
+     * @param recovered
      */
     static void DeserializeAckPacket(void *packetBytes, AckPacket **recovered);
 };
--- src/BinarySerializer.cpp
+++ src/BinarySerializer.cpp
@@ -6,29 +6,25 @@
 #include <iostream>
 #include "BinarySerializer.h"
 
-void BinarySerializer::SerializeDataPacket(DataPacket *data_pack, void **ptr)
+void BinarySerializer::SerializeDataPacket(DataPacket *data_pack, void **projected_data_ptr)
 {
-    (*ptr) = reinterpret_cast<void *>(data_pack);
+    (*projected_data_ptr) = reinterpret_cast<void *>(data_pack);
 }
 
 void BinarySerializer::DeserializeDataPacket(void *packetBytes, unsigned int packet_len,
         DataPacket **recovered)
 {
     (*recovered) = reinterpret_cast<DataPacket *>(packetBytes);
-    //std::cout << "Unpacked:" << (*recovered)->data << std::endl;
 }
 
-void BinarySerializer::SerializeAckPacket(AckPacket *ack_pack, void **ptr)
+void BinarySerializer::SerializeAckPacket(AckPacket *ack_pack, void **projected_ack_ptr)
 {
-    (*ptr) = reinterpret_cast<void *>(ack_pack);
+    (*projected_ack_ptr) = reinterpret_cast<void *>(ack_pack);
 }
 
 void BinarySerializer::DeserializeAckPacket(void *packetBytes,
         AckPacket **recovered)
 {
     (*recovered) = reinterpret_cast<AckPacket *>(packetBytes);
-//    std::cout << "Deserializer Len:" << (*recovered)->len << std::endl;
-//    std::cout << "Deserializer Checksum:" << (*recovered)->chksum << std::endl;
-//    std::cout << "Deserializer ACK NUM:" << (*recovered)->ack_num << std::endl;
 }
 
